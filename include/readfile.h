389689b6fdb33f331f964a5386211f68a1aa14de
guyupan1911 <guyupan1911@gmail.com>
2020-01-19 11:02:07 +0800

    nothing

--- include/readfile.h
+++ include/readfile.h
@@ -1,3 +1,20 @@
+/*
+读取数据的函数
+eigen_T: 变换矩阵
+        [R, t
+         0, 1]
+
+eigen_Cam_intrin: 相机内参
+        [fx, 0, cx
+          0,fy, cy
+          0, 0, 1]
+
+eigen_Cam_dist: 相机畸变参数；
+        [k1, k2, k3, k4, k5]
+
+lidar_points: 点云数据集
+*/
+
 #ifndef READFILE_H
 #define READFILE_H
 
@@ -7,10 +24,27 @@
 #include <opencv2/core/eigen.hpp>
 #include "pointXYZRGB.h"
 
-Eigen::Matrix4d eigen_T;
-Eigen::Matrix3d eigen_Cam_intrin;
-Eigen::Matrix<double, 5, 1> eigen_Cam_dist;
-std::vector<pointXYZRGB> lidar_points;
+extern Eigen::Matrix4d eigen_T;
+extern Eigen::Matrix3d eigen_Cam_intrin;
+extern Eigen::Matrix<double, 5, 1> eigen_Cam_dist;
+extern std::vector<pointXYZRGB> lidar_points;
+
+//从txt文件中读取点云数据，输入文件名; 返回元素为点云的向量。
+std::vector<pointXYZRGB> readpointcloud( std::string& filename );
+
+
+//保存点云数据到txt文件。
+void savepointcloud(std::vector<pointXYZRGB>& lidar_points);
+
+//读取激光雷达到相机的外参，返回4×4矩阵。
+Eigen::Matrix4d readCalibLidarToCam(std::string& filename);
+
+//读取相机内参，返回3×3矩阵。
+Eigen::Matrix3d readCamintrin(std::string& filename)；
+
+//读取相机畸变系数，返回5*1矩阵。
+Eigen::Matrix<double, 5, 1> readCamdist(std::string& filename)；
+
 
 std::vector<pointXYZRGB> readpointcloud( std::string& filename )
 {   
@@ -109,6 +143,4 @@ Eigen::Matrix<double, 5, 1> readCamdist(std::string& filename)
     cv::cv2eigen(cv_CamDist, eigen_Cam_dist);
     return eigen_Cam_dist;
 }
-
-
 #endif
